d9eb64f18df4e75f329bbb8657b8e06943bb5148
Your Name <you@example.com>
2021-01-06 11:14:45 -0800

    Got Javascript to work, as well as negative numbers to be red. Working on CSS next.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-let subtractButton = document.getElementById("subtract_button") // This step is actually unnecessary because HTML5 recognizes DOM elements based on their ID name, as long as their ID name has no hyphens in it. Underscores are acceptable.
+// Note: We no longer need to say
+// let subtractButton = document.getElementById("subtract_button") 
+// Because HTML5 is able to recognize DOM elements by just their ID, as long as their ID name has no hyphens in it. Underscores are acceptable.
+
+let count = parseInt(count_div.innerText)
+let inputValue = input_box.value
+
+subtract_button.addEventListener('click', (event) => {
+    inputValue = input_box.value
+    math('subtract')
+    event.preventDefault()
+})
 
 add_button.addEventListener('click', (event) => {
+    inputValue = input_box.value
+    math('add')
     event.preventDefault()
-    console.log("Sup")
 })
+
+function math(operator) {
+    switch(operator) {
+        case('add'): 
+            count += parseInt(inputValue) 
+            break
+        case('subtract'):
+            count -= parseInt(inputValue)
+            break
+    }
+    count_div.innerText = count
+    if (count < 0) {count_div.style.color = 'red' }
+    else { count_div.style.color = 'black' }
+}
